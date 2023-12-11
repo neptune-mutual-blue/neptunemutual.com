1e53227dec0fd8b06610bc350c264d42fdf586c2
Ankit Kumar Karna <ankitkarna.dev@gmail.com>
2023-12-11 18:50:12 +0545

    Updated USDC address

--- src/data/networks.ts
+++ src/data/networks.ts
@@ -68,7 +68,7 @@ const tokens = {
   },
   polygon: {
     npm: '0x57f12FE6A4e5fe819eec699FAdf9Db2D06606bB4',
-    usdc: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+    usdc: '0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359'
   }
 }
 
@@ -78,30 +78,30 @@ const swaps: Array<{
   title: string
   href: string
 }> = [
-  {
-    src: import('../elements/icons/custom/sushi-arb.svg?raw').then((x: any) => x.default),
-    srcDark: import('../elements/icons/custom/sushi-arb-dark.svg?raw').then((x: any) => x.default),
-    title: 'Sushiswap (Arbitrum)',
-    href: `https://www.sushi.com/swap?token0=${tokens.arb.usdc}&token1=${tokens.arb.npm}&chainId=42161`
-  },
-  {
-    src: import('../elements/icons/custom/sushi-bsc.svg?raw').then((x: any) => x.default),
-    srcDark: import('../elements/icons/custom/sushi-bsc-dark.svg?raw').then((x: any) => x.default),
-    title: 'Sushiswap (BNB Chain)',
-    href: `https://www.sushi.com/swap?token0=${tokens.bsc.usdc}&token1=${tokens.bsc.npm}&chainId=56`
-  },
-  {
-    src: import('../elements/icons/custom/sushi-bsc.svg?raw').then((x: any) => x.default),
-    srcDark: import('../elements/icons/custom/sushi-bsc-dark.svg?raw').then((x: any) => x.default),
-    title: 'Sushiswap (Polygon)',
-    href: `https://www.sushi.com/swap?token0=${tokens.polygon.usdc}&token1=${tokens.polygon.npm}&chainId=137`
-  },
-  {
-    src: import('../elements/icons/custom/uniswap-eth.svg?raw').then((x: any) => x.default),
-    srcDark: import('../elements/icons/custom/uniswap-eth-dark.svg?raw').then((x: any) => x.default),
-    title: 'Uniswap (Ethereum)',
-    href: `https://app.uniswap.org/#/swap?inputCurrency=USDC&outputCurrency=${tokens.eth.npm}&chain=mainnet`
-  }
-]
+    {
+      src: import('../elements/icons/custom/sushi-arb.svg?raw').then((x: any) => x.default),
+      srcDark: import('../elements/icons/custom/sushi-arb-dark.svg?raw').then((x: any) => x.default),
+      title: 'Sushiswap (Arbitrum)',
+      href: `https://www.sushi.com/swap?token0=${tokens.arb.usdc}&token1=${tokens.arb.npm}&chainId=42161`
+    },
+    {
+      src: import('../elements/icons/custom/sushi-bsc.svg?raw').then((x: any) => x.default),
+      srcDark: import('../elements/icons/custom/sushi-bsc-dark.svg?raw').then((x: any) => x.default),
+      title: 'Sushiswap (BNB Chain)',
+      href: `https://www.sushi.com/swap?token0=${tokens.bsc.usdc}&token1=${tokens.bsc.npm}&chainId=56`
+    },
+    {
+      src: import('../elements/icons/custom/sushi-bsc.svg?raw').then((x: any) => x.default),
+      srcDark: import('../elements/icons/custom/sushi-bsc-dark.svg?raw').then((x: any) => x.default),
+      title: 'Sushiswap (Polygon)',
+      href: `https://www.sushi.com/swap?token0=${tokens.polygon.usdc}&token1=${tokens.polygon.npm}&chainId=137`
+    },
+    {
+      src: import('../elements/icons/custom/uniswap-eth.svg?raw').then((x: any) => x.default),
+      srcDark: import('../elements/icons/custom/uniswap-eth-dark.svg?raw').then((x: any) => x.default),
+      title: 'Uniswap (Ethereum)',
+      href: `https://app.uniswap.org/#/swap?inputCurrency=USDC&outputCurrency=${tokens.eth.npm}&chain=mainnet`
+    }
+  ]
 
 export { networks, swaps }
